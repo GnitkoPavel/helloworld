d0922c809017c5ad90cec78dd06737b178622e73
Test1 <pavel.gnitko@ukr.net>
2018-10-22 16:26:22 +0300

    asdasdasd

--- src/main/webapp/myClass.java
+++ src/main/webapp/myClass.java
@@ -1,12 +1,5 @@
 public class myClass
 {
-    Arrays.asList("a1", "a2", "b1", "c2", "c1").stream()
-    .filter(...)
-    .forEach(...);
-
-    Arrays.asList(1, 2, 3, 4).stream() // Noncompliant
-    .filter(...)
-    .forEach(...);
     private static myObject1 myObj1;
     private static mutex1 = new Object();
     private static myObject2 myObj2;
